7e8732d3fe20cd040503a72f7914c19f49456f6c
Kiminjo <deulseong1121@gmail.com>
2023-09-18 17:22:53 +0900

    [Fix] Fix Library

--- train_clf.py
+++ train_clf.py
@@ -1,5 +1,5 @@
-from data import  TrainDataset, ValidDataset, InferenceDataset, DEFAULT_SIZE
-from models import PatchCore, load_model
+from data import  TrainDataset, ValidDataset, DEFAULT_SIZE
+from models import load_model
 from utils import compute_mask, extract_features
 from utils import SplittedMaskInfo, backbones, dataset_scale_factor
 
@@ -13,53 +13,12 @@ from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import joblib
-from lightgbm import LGBMClassifier
 from sklearn.metrics import accuracy_score, f1_score
 from typing import List, Tuple
+import random 
 import time
 
-def patchcore_train(train_loader: DataLoader,
-                    valid_loader: DataLoader,
-                    backbone: str = 'WideResNet50',
-                    checkpoint_path: str = "checkpoints/weight.pt",
-                    f_coreset: float = 0.1
-                    ) -> None:
-
-    # Vanilla or Clip version
-    vanilla = backbone == "WideResNet50"
-
-    results = {}    # Image level ROC-AUC, Pixel level ROC-AUC
-    if vanilla:
-        size = DEFAULT_SIZE
-    elif backbone == 'ResNet50':    # RN50
-        size = 224
-    elif backbone == 'ResNet50-4':  # RN50x4
-        size = 288
-    elif backbone == 'ResNet50-16': # RN50x16
-        size = 384
-    elif backbone == 'ResNet101':   # RN50x101
-        size = 224
-    else:
-        raise Exception('You can use the following nets: ResNet50, ResNet50-4, ResNet50-16, ResNet50-64, ResNet101')
-
-    print(f'Running PatchCore...')
-    patch_core = PatchCore(f_coreset, 
-                           vanilla=vanilla, 
-                           backbone=backbones[backbone], 
-                           image_size=size)            
-    print(f'Training...')
-    patch_core.fit(train_loader, scale=dataset_scale_factor[backbone])
-
-    print(f'Testing...')
-    image_rocauc, pixel_rocauc = patch_core.evaluate(valid_loader)
-
-    print(f'Results:')
-    results["image_rocauc"] = float(image_rocauc) 
-    results["pixel_rocauc"] = float(pixel_rocauc)
-    print(f'- Image-level ROC AUC = {image_rocauc:.3f}')
-    print(f'- Pixel-level ROC AUC = {pixel_rocauc:.3f}\n')
-
-    patch_core.save(save_path=checkpoint_path)
+random.seed(21)
 
 def patchcore_predict(model,
                       loader: DataLoader,
@@ -107,7 +66,7 @@ def split_mask(mask: np.array,
                area_threshold: int = 50
                ): 
     object_infos = [] 
-    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(mask, connectivity=8)
+    num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(mask, connectivity=8)
 
     for label in range(1, num_labels):
         area = stats[label, cv2.CC_STAT_AREA]
@@ -135,7 +94,6 @@ def get_objects_feature(feature: List[torch.tensor],
 
 
 if __name__=='__main__':
-    checkpoint = "checkpoints/weight.pt"
     data_dir = Path("datasets/bottle/")
     defects = ["good", "broken_large", "broken_small", "contamination"]
     
@@ -160,22 +118,15 @@ if __name__=='__main__':
     valid_loader = DataLoader(validset,
                               num_workers=8,
                               shuffle=False)
-
-
-    # # Component 1. Train PatchCore Model 
-    # checkpoint = "checkpoints/train_test.pt"
-    # patchcore_train(train_loader=train_loader,
-    #                 valid_loader=valid_loader,
-    #                 checkpoint_path=checkpoint)
     
-    # Component 2. Load Model and Inference 
-    patchcore_checkpoint = "checkpoints/train_test.pt"
+    # Component 1. Load Model and Inference 
+    patchcore_checkpoint = "checkpoints/split_pipeline.pt"
     features, prediction_masks, anomaly_scores = patchcore_inference(loader=valid_loader,
                                                                      checkpoint=patchcore_checkpoint)
     
-    # Component 3. Split Mask per objects
-    # Component 4. Get Feature of object 
-    rf_checkpoint = "checkpoints/rf.pkl"
+    # Component 2. Split Mask per objects
+    # Component 3. Get Feature of object 
+    rf_checkpoint = "checkpoints/rf_test.pkl"
     X, y = [], []
     print("Feature Process...")
     for feature, pred_mask, anomaly_score, cls_label in tqdm(zip(features, prediction_masks, anomaly_scores, valid_labels), total=len(features)): 
@@ -188,7 +139,7 @@ if __name__=='__main__':
     X = np.vstack(X) 
     y = np.array(y)
 
-    # Component 5. Train Random Forest Classifier 
+    # Component 4. Train Random Forest Classifier 
     print("Random Forest Training...")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     clf = RandomForestClassifier(n_estimators=300,
@@ -209,5 +160,4 @@ if __name__=='__main__':
     print(y_test)
     print(rf_result)
 
-    # Save Boosting Model
     joblib.dump(clf, rf_checkpoint)
